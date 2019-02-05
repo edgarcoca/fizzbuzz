c7eb0668417c0865acefbce33c36023be1e73178
Edgar Coca <edgar.coca@atrapalo.com>
2019-02-05 17:27:46 +0100

    v1

--- src/Fizzbuzz.php
+++ src/Fizzbuzz.php
@@ -2,8 +2,24 @@
 
 class Fizzbuzz
 {
-    public function convert($param)
+    public function convert(int $param)
     {
-        return 1;
+        if($param <= 0 || $param >= 101){
+            throw new InvalidArgumentException($param." isn't a valid number");
+        }
+
+        if($param%15 == 0){
+            return 'FizzBuzz';
+        }
+
+        if($param%3 == 0){
+            return 'Fizz';
+        }
+
+        if($param%5 == 0){
+            return 'Buzz';
+        }
+
+        return $param;
     }
 }
--- tests/FizzbuzzTest.php
+++ tests/FizzbuzzTest.php
@@ -17,4 +17,65 @@ class FizzbuzzTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(1, $this->sut->convert(1));
     }
 
+    /**
+     * @test
+     */
+    public function twoMustReturnTwo()
+    {
+        $this->assertEquals(2, $this->sut->convert(2));
+    }
+
+    /**
+     * @test
+     */
+    public function multipleOfThreeMustReturnFizz()
+    {
+        $this->assertEquals('Fizz', $this->sut->convert(6));
+    }
+
+    /**
+     * @test
+     */
+    public function multipleOfFiveMustReturnBuzz()
+    {
+        $this->assertEquals('Buzz', $this->sut->convert(10));
+    }
+
+    /**
+     * @test
+     */
+    public function multipleOfThreeAndFiveMustReturnFizzBuzz()
+    {
+        $this->assertEquals('FizzBuzz', $this->sut->convert(15));
+        $this->assertEquals('FizzBuzz', $this->sut->convert(30));
+        $this->assertEquals('FizzBuzz', $this->sut->convert(45));
+    }
+
+    /**
+     * @test
+     */
+    public function numberUnderOneMustFail()
+    {
+        $this->expectException(InvalidArgumentException::Class);
+        $this->sut->convert(0);
+    }
+
+    /**
+     * @test
+     */
+    public function numberOverOneHundredMustFail()
+    {
+        $this->expectException(InvalidArgumentException::Class);
+        $this->sut->convert(101);
+    }
+
+    /**
+     * @test
+     */
+    public function paramTypeNotEqualsIntegerMustFail()
+    {
+        $this->expectException(TypeError::Class);
+        $this->sut->convert('E');
+    }
+
 }
